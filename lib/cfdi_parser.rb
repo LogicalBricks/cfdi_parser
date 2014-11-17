9c2f5ab2c1cf35be28d900a137df04453f300a4b
hermes-logicalbricks <hermes.ojeda@logicalbricks.com>
2014-11-17 12:00:38 -0600

    Added first tests

--- lib/cfdi_parser.rb
+++ lib/cfdi_parser.rb
@@ -3,8 +3,38 @@ require 'nokogiri'
 
 module CfdiParser
   class CfdiParser
+    attr_accessor :doc
+
     def initialize(cfdi)
-      doc = Nokogiri::XML(cfdi)
+      @doc = Nokogiri::XML(cfdi)
+      @namespaces = generate_namespaces
+    end
+
+    def version
+      doc.xpath("//cfdi:Comprobante",namespaces).attribute('version').value rescue nil
+    end
+
+    def total
+      doc.xpath("//cfdi:Comprobante",namespaces).attribute('total').value rescue nil
+    end
+
+    def subtotal
+      doc.xpath("//cfdi:Comprobante",namespaces).attribute('subTotal').value rescue nil
+    end
+
+    private
+
+    def namespaces
+      @namespaces
+    end
+
+    def generate_namespaces
+      namespaces = doc.collect_namespaces
+      namespaces_names = {}
+      namespaces.each_pair do |key, value|
+        namespaces_names[key.sub(/^xmlns:/, '')] = value
+      end
+      namespaces_names
     end
   end
 end
--- test/cfdi_parser_test.rb
+++ test/cfdi_parser_test.rb
@@ -7,7 +7,19 @@ class CfdiParserTest < MiniTest::Test
     @cfdi_parser = CfdiParser::CfdiParser.new(file)
   end
 
-  def test_valid_object
+  def test_object_not_nil
     assert_equal @cfdi_parser.nil?, false
+  end
+
+  def test_get_version
+    assert_equal @cfdi_parser.version, "3.2"
+  end
+
+  def test_get_total
+    assert_equal @cfdi_parser.total, "116.00"
+  end
+
+  def test_get_subtotal
+    assert_equal @cfdi_parser.subtotal, "100.00"
   end
 end
